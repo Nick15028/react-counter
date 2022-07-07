e4beffdf5e49e08e9d0743eb521a6275a6a083d7
Nick Chang <rich0432@gmail.com>
2022-07-07 11:25:08 +0800

    finish 1.0

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('')
+  const [count, setCount] = useState(0)
+  // useEffect(() => {
+  //   console.log("useEffect", name)
+  // }, [name])
+
+  useEffect(() => {
+    console.log('useEffect count', count)
+    const myCount = setInterval(() => {
+      setCount(count)
+    }, 1000)
+    return () => {
+      clearInterval(myCount)
+      console.log('clearIntercal')
+    }
+  }, [count])
+
+  // const changeName = () => {
+  //   console.log("在 changeName function 內", name)
+  //   setName('Nick')
+  //   console.log("在 changeName function 內，setName 之後", name)
+  // }
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{console.log("render 裡面", count)}</Text>
+      <Text>{count}</Text>
+      {/* <Button
+        title={"change name"}
+        onPress={() => changeName()}
+      /> */}
     </View>
   );
 }
